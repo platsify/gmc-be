b4ba9e533f328029aee176c3aa0d1e6012bdf1bf
Tran Hoang Giang <tranhoanggiang@cryptopie-labo.com>
2021-10-28 18:22:39 +0700

    Setup helper

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['hello' => 'haha'], 201);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
