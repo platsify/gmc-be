001c43a9a4823e3b09ea4798a3c838eaeb57dd68
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2021-11-03 02:32:43 +0700

    Merge product and product map into single table

--- database/migrations/2021_10_28_112614_create_products_table.php
+++ database/migrations/2021_10_28_112614_create_products_table.php
@@ -18,6 +18,11 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->string('url')->nullable();
             $table->string('image_url')->nullable();
+            $table->string('original_product_id')->index('INDEX_ORIGINAL_PRODUCT_ID');
+            $table->integer('original_last_update');
+            $table->integer('category_id')->index('INDEX_CATEGORY_ID');
+            $table->boolean('sync_gmc')->default(false)->comment('Does this product synced to GMC?');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
