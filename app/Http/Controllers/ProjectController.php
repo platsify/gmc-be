bf2378d4dbd750436760055b35c6b08e6a398d6e
PC <profile.nvt@gmail.com>
2021-11-10 20:48:36 +0700

    Tìm products theo categories khi tạo project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\FindProductsFromCategories;
 use App\Jobs\SyncShopbase;
+use App\Models\Product;
+use App\Models\ProductMapCategory;
+use App\Models\ProductMapProjects;
 use App\Models\Project;
 use App\Repositories\Product\ProductRepositoryInterface;
 use App\Repositories\Project\ProjectRepositoryInterface;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProjectController extends Controller
 {
-    private $productRepository;
+    protected $productRepository;
     private $projectRepository;
+
     public function __construct(ProductRepositoryInterface $productRepository, ProjectRepositoryInterface $projectRepository)
     {
         $this->productRepository = $productRepository;
@@ -25,7 +31,7 @@ class ProjectController extends Controller
      *
      * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $perPage = isset($request->per_page) ? (int)$request->per_page : 10;
         $sort = $request->sort ?? 'id';
@@ -54,7 +60,7 @@ class ProjectController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -75,9 +81,10 @@ class ProjectController extends Controller
         }
         if ($project) {
             $result = $this->projectRepository->update($request->_id, $request->all());
-            return response()->json(['status' => 'success', 'data' => $result, 'message' => "Cập nhật thành cônffg"]);
+            return response()->json(['status' => 'success', 'data' => $result, 'message' => "Cập nhật thành công"]);
         } else {
             $result = $this->projectRepository->create($request->all());
+            FindProductsFromCategories::dispatch($result->_id, $request->categories);
             return response()->json(['status' => 'success', 'data' => $result, 'message' => "Thêm thành công"]);
         }
     }
@@ -86,7 +93,7 @@ class ProjectController extends Controller
      * Display the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -97,7 +104,7 @@ class ProjectController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -109,7 +116,7 @@ class ProjectController extends Controller
      *
      * @param Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
