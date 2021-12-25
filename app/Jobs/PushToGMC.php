b42a37ab09922e9058b25692e79ba09fd5adc6fe
Tran Hoang Giang <tranhoanggiang@cryptopie-labo.com>
2021-12-25 22:24:02 +0700

    Ha job

--- app/Jobs/PushToGMC.php
+++ app/Jobs/PushToGMC.php
@@ -52,7 +52,7 @@ class PushToGMC implements ShouldQueue
         shuffle($activeProjects);
         $projectId = $activeProjects[0];
         echo 'Push project '.$projectId."\n";
-        $maps = ProductMapProjects::where('project_id', $projectId)->where('synced', false)->limit(4000)->get();
+        $maps = ProductMapProjects::where('project_id', $projectId)->where('synced', false)->limit(3000)->get();
         if (!$maps) {
             //echo 'Het map roi' . "\n";
             return;
