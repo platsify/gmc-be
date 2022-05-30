064bccd13996237e2a94b734c82fd1b150fd41a1
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2022-05-31 00:54:03 +0700

    Cho phep size co chua 6

--- app/Jobs/PushToGMC.php
+++ app/Jobs/PushToGMC.php
@@ -493,9 +493,9 @@ class PushToGMC implements ShouldQueue
                         }
                     }
 
-                    // Loại bỏ các sản phẩm có Size nhưng ko phải 'S', 'Throw', 'Tween', 'Twin', '3'
-                    if ($sizeOption != 99999 && !in_array(mb_strtolower($size), ['s', 'throw', 'tween', 'twin', '3'])) {
-                        echo 'Size = ' . $size . " khong thuoc danh sach cho phep ['S', 'Throw', 'Tween', 'Twin', '3'] nen bo qaaua \n";
+                    // Loại bỏ các sản phẩm có Size nhưng ko phải 'S', 'Throw', 'Tween', 'Twin', có chứa 6
+                    if ($sizeOption != 99999 && !in_array(mb_strtolower($size), ['s', 'throw', 'tween', 'twin']) && strpos(size, '6') === false) {
+                        echo 'Size = ' . $size . " khong thuoc danh sach cho phep ['S', 'Throw', 'Tween', 'Twin', 'có chứa 6'] nen bo qaaua \n";
                         continue;
                     }
                     echo 'SSSSSSize = ' . $size . " =====\n";
